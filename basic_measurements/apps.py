3a040a3d7496fa4bcd7d6431801a601cfdd81c8f
harpykaur16 <harpykaur16@gmail.com>
2022-07-24 18:18:43 +0530

    first model added

--- basic_measurements/apps.py
+++ basic_measurements/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BasicMeasurementsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'basic_measurements'
+    verbose_name = 'Measurement between 2 locations'
